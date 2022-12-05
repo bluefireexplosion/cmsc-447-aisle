87406f062bd1f89abb0ecb81673292d644ae61e9
bluefireexplosion <zacha9020@gmail.com>
2022-12-05 15:59:33 -0500

    Fixed windows build script to have more verbose instructions and warning about needing xlaunch running

--- build_windows.ps1
+++ build_windows.ps1
@@ -1,6 +1,10 @@
+echo "Insure Xlaunch is running before running this or it won't work"
+echo "Xlaunch is necessary for Windows"
 docker build -t electron-wrapper .\docker
+echo "INSURE XLAUNCH is RUNNING before running this or it won't work"
+echo "Xlaunch is necessary for Windows"
 wsl ip route show
-$Server = Read-Host -Prompt 'Input the IP above plus a :0.0 at the end:'
+$Server = Read-Host -Prompt 'Input the IP at the top of the IPs above plus a :0.0 at the end:'
 set-variable -name DISPLAY -value $Server
 docker run -ti --rm -e DISPLAY=$DISPLAY --rm -it electron-wrapper bash
 echo "Now entering devenv via docker..."
